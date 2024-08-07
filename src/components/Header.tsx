488bf7785e46294fb1b5aef27ab7c28aa58b7580
Ntrv3390 <78497133+Ntrv3390@users.noreply.github.com>
2024-08-07 15:43:48 +0530

    Updated: Header Component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,19 +1,41 @@
 import Link from "next/link";
+import { Search, ExternalLink } from "lucide-react";
 
 const Header = () => {
   return (
     <div className="flex fixed w-full px-3 justify-between items-center p-2 bg-slate-800 text-white shadow-md">
-      <Link className="font-bold text-xl" href={"/"}>ArrayIndexOutOfBound</Link>
-      <div className="flex items-center justify-center w-[80rem] gap-3">
-        <input type="text" placeholder="Enter your query..." className="py-1 w-3/4 px-3 font-light text-slate-800 rounded-md"/>
-        <button className="py-0 ml-[-6rem] px-3 rounded-md bg-green-800">Search</button>
+      <Link className="font-bold text-xl" href={"/"}>
+        ArrayIndexOutOfBound
+      </Link>
+      <div className="flex items-center justify-center w-[80rem]">
+        <input
+          type="text"
+          placeholder="java, python, c++, js, php and more..."
+          className="py-1 w-3/4 px-3 font-light text-slate-800 rounded-sm"
+        />
+        <button className="py-0 ml-[-4rem] px-3">
+          <Search className="text-slate-800 h-5 w-5" />
+        </button>
       </div>
       <ul className="flex items-center gap-3 justify-center ">
-        <li className="py-1 px-3 rounded-md"><Link href={"/login"}>Home</Link></li>
-        <li className="py-1 px-3 rounded-md"><Link href={"/login"}>About</Link></li>
-        <li className="py-1 px-3 rounded-md"><Link href={"/login"}>Login</Link></li>
-        <li className="py-1 px-3 rounded-md"><Link href={"/login"}>Register</Link></li>
-        <li className="bg-green-800 py-1 px-3 rounded-md"><Link href={"https://github.com/Ntrv3390/ArrayIndexOutOfBound"}>Contribute</Link></li>
+        <li className="py-1 px-3 rounded-sm">
+          <Link className="hover:text-green-600" href={"/login"}>Home</Link>
+        </li>
+        <li className="py-1 px-3 rounded-sm">
+          <Link className="hover:text-green-600 " href={"/login"}>About</Link>
+        </li>
+        <li className="py-1 px-3 rounded-sm">
+          <Link className="hover:text-green-600 " href={"/login"}>Login</Link>
+        </li>
+        <li className="py-1 px-3 rounded-sm">
+          <Link className="hover:text-green-600 " href={"/login"}>Register</Link>
+        </li>
+        <li className="bg-green-800 hover:bg-green-600 flex py-1 px-3 rounded-sm">
+          <Link className="flex gap-2" href={"https://github.com/Ntrv3390/ArrayIndexOutOfBound"}>
+            Contribute
+            <ExternalLink className="w=5 h-5 text-white" />
+          </Link>
+        </li>
       </ul>
     </div>
   );
